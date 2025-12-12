afa12255b8aaae012d0fbd4a4ae841414630eb29
kavonraggs <kavonraggs@gmail.com>
2025-12-12 15:30:00 -0600

    changed easy shop to recordshop

--- backend-api/src/main/java/org/yearup/RecordShopAPI.java
+++ backend-api/src/main/java/org/yearup/RecordShopAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyshopApplication
+public class RecordShopAPI
 {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(EasyshopApplication.class);
+        SpringApplication app = new SpringApplication(RecordShopAPI.class);
         app.run(args);
     }
 }
